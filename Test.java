84dfbf1eaa0b9f8d29fdfc795e689b3de672087f
Emma Buller <dib5017@gmail.com>
2020-12-06 19:15:11 -0500

    Tested hasNext

--- Test.java
+++ Test.java
@@ -20,5 +20,15 @@ public class Test {
     x.reset();
     System.out.println("reset current value");
     System.out.println(x.next());
+    System.out.println();
+    System.out.println("Loop thing");
+    IntegerSequence r = new Range(10,15);
+    while(r.hasNext()){
+      System.out.print(r.next());
+      if( r.hasNext() ){
+         System.out.print( ", " );
+      }
+    }
+    System.out.println();
   }
 }
