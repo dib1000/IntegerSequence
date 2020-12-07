8af0f264afe6e1d4f1da4a2c4a63dc01ae14e576
Emma Buller <dib5017@gmail.com>
2020-12-06 19:09:32 -0500

    Added next method (w/o exception)

--- Test.java
+++ Test.java
@@ -1,9 +1,21 @@
 public class Test {
   public static void main(String[] args) {
+    System.out.println("TESTING LENGTH AND CONSTRUCTOR EXCEPTION");
     IntegerSequence x = new Range(10,20); //works
     System.out.println(x.length());
     IntegerSequence y = new Range(10,10); //works
     System.out.println(y.length());
-    //Range z = new Range(11,10); //should throw error
+    try {
+        Range z = new Range(11,10);
+    }
+    catch(IllegalArgumentException e) {
+      e.printStackTrace();
+    }
+    System.out.println();
+
+    System.out.println("TESTING ALL OF THE OTHER METHODS");
+    System.out.println(x.next());
+    System.out.println(x.next());
+    System.out.println(x.next());
   }
 }
