abfd797bdd4ecdc3d4b095cee254490c893d8553
Emma Buller <dib5017@gmail.com>
2020-12-07 12:34:14 -0500

    Added first constructor

---
+++ ArraySequence.java
@@ -0,0 +1,23 @@
+import java.util.NoSuchElementException;
+public class ArraySequence implements IntegerSequence{
+  int currentIndex;
+  int[] data;
+
+  /*Construct the sequence by copying values from the other array into the data array*/
+  public ArraySequence(int [] other){
+    data = other;
+    currentIndex = 0;
+  }
+  public boolean hasNext() {
+    return true;
+  }
+  public int next() {
+    return 0;
+  }
+  public int length() {
+    return 0;
+  }
+  public void reset() {
+    System.out.println();
+  }
+}
---
+++ ArrayTester.java
@@ -0,0 +1,6 @@
+public class ArrayTester {
+  public static void main(String[] args) {
+    int[]nums = {1,3,5,0,-1,3,9};
+    IntegerSequence as = new ArraySequence(nums);
+  }
+}
