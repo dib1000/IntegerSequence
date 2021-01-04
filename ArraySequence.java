7f5b314d87db45f683bfeb272c37ff99545666ee
Emma Buller <dib5017@gmail.com>
2021-01-03 22:54:09 -0500

    Got rid of external array reference

--- ArraySequence.java
+++ ArraySequence.java
@@ -5,7 +5,10 @@ public class ArraySequence implements IntegerSequence{
 
   /*Construct the sequence by copying values from the other array into the data array*/
   public ArraySequence(int [] other){
-    data = other;
+    data = new int[other.length];
+    for(int i = 0; i<other.length; i++) {
+      data[i] = other[i];
+    }
     currentIndex = 0;
   }
   //Postcondition: The otherseq will be reset.
